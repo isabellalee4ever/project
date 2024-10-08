0e9d9d095c0ae4f608a64e91c9d6788118ac3fd6
isabellalee4ever <888isabellalee@gmail.com>
2024-10-08 00:31:20 +0000

    coffee!

--- coffeeshop.py
+++ coffeeshop.py
@@ -1,1 +1,15 @@
+menu={
+   "frappuccino":5.00,
+   "cappuccino":4.30,
+   "hot chocolate":4.00,
+   "choclate chip cookie":3.00,
+   "sugar cookie": 2.00,
+   "latte":5.50,
+   "iced tea":4.00,
+   "brownie":2.50,
+}
+# print(menu)
 
+# order=input("what would you like to order? Type exit to exit")
+for i in menu:
+    print(i,": $"+str(menu[i]))
