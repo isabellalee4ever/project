fcd9ce4d1906cf46a3d268d440a5fa49754d8a33
isabellalee4ever <888isabellalee@gmail.com>
2024-10-08 00:05:10 +0000

    whatever

--- numberguesser.py
+++ numberguesser.py
@@ -1,17 +1,28 @@
-import random
+import random,time
+
+def get_imput_from_user():
+    s=time.time()
+    guess=int(input("guess number between 1-200: "))
+    e=time.time()
+    return e-s, guess
+
 
 mystery=random.randint(1,200)
+counter=1
 
-guess=int(input("guess number between 1-200: "))
+t,response=get_imput_from_user()
 
-while guess!=mystery:
 
-    if guess>mystery:
+while response!=mystery:
+    counter+=1
+    if t>5:
+        print("You took too much time to respond")
+    elif response>mystery:
         print("too large")
     else:
         print("too small")
     
-
-    guess=int(input("guess number between 1-200: "))
+    t,response=get_imput_from_user()
 
 print("You Win!")
+print("you took",counter,"tries")
