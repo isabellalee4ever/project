61f1f0d45cb0f187bb31fc7cf5bf48ca7acf4536
isabellalee4ever <888isabellalee@gmail.com>
2024-10-15 00:31:04 +0000

    coffee

--- coffeeshop.py
+++ coffeeshop.py
@@ -2,7 +2,7 @@ menu={
    "frappuccino":5.00,
    "cappuccino":4.30,
    "hot chocolate":4.00,
-   "choclate chip cookie":3.00,
+   "chocolate chip cookie":3.00,
    "sugar cookie": 2.00,
    "latte":5.50,
    "iced tea":4.00,
@@ -11,5 +11,22 @@ menu={
 # print(menu)
 
 # order=input("what would you like to order? Type exit to exit")
+print("menu")
 for i in menu:
     print(i,": $"+str(menu[i]))
+order=input("what would you like to order? Type exit to exit ")
+price=0
+reciept={}
+while order!="exit":
+   num=int(input("how many do you want to order?"))
+   price+=menu[order]*num 
+   tempprice=menu[order]*num
+   reciept[order,"x",num]=tempprice
+   order=input("what would you like to order? Type exit to exit ")
+reciept["subtotal"]=price
+print("your total price is $", price)
+print("reciept")
+for i in reciept:
+   print(i,": $"+str(reciept[i]))
+
+
